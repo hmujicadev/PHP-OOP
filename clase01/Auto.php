ab88cc03dcb5398b25708cfe956615dc1886f620
hmujicadev <hmujicadev@gmail.com>
2020-01-29 20:30:21 -0300

    first commit

---
+++ clase01/Auto.php
@@ -0,0 +1,47 @@
+<?php
+    
+    class Auto {
+
+       private $marca;
+       private $modelo;
+       private $precio;
+
+       public function verDatos(){
+
+       }
+
+       public function listarAutos(){
+
+      }
+
+      public function verAutosPorId(){
+
+      }
+
+      public function agregarAuto(){
+
+      }
+
+      public function modificarAuto(){
+
+      }
+
+      public function eliminarAuto(){
+
+      }
+
+
+      public function setMarca($marca){
+
+        $this->marca = $marca;
+
+      }
+
+
+      public function getMarca(){
+
+        return $this->marca;
+
+      }
+
+    }
